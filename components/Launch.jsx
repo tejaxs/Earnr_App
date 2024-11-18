42cf26c189b76b56921539a6a9eed2c3fa8df14e
Yashrd2002 <94217776+Yashrd2002@users.noreply.github.com>
2024-11-18 21:33:09 +0530

    v99999999

--- components/Launch.jsx
+++ components/Launch.jsx
@@ -10,7 +10,7 @@ const Launch = ({ open, handleClose }) => {
       aria-describedby="modal-modal-description"
       className=" flex justify-center items-center"
     >
-      <div className="flex flex-col gap-3  poppins-400 absolute z-10 h-2/6 bg-white text-black md:w-[350px] w-10/12 rounded-xl p-4 border-none">
+      <div className="flex flex-col items-center justify-center gap-3  poppins-400 absolute z-10 h-[160px] bg-white text-black md:w-[350px] w-10/12 rounded-xl p-4 border-none">
         <h2 className="text-[20px] text-[#FFCD48] poppins-600">Earnr</h2>
         <p className="text-sm">
            You can open it by tapping
